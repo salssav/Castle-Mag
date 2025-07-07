70647089c219529c3a10c979934ae8070bd88411
salssav <valevelapra@gmail.com>
2025-07-07 13:41:59 +0100

    adding pics to draggable and changes ascii page

--- js/asciiArt.js
+++ js/asciiArt.js
@@ -88,53 +88,61 @@ window.addEventListener('resize', resizeCanvas);
 /* DRAGGABLE PHOTOS */
 
 // Make the  elements draggable:
-dragElement(document.getElementById("draggable"));
-dragElement(document.getElementById("draggable_2"));
-dragElement(document.getElementById("draggable_3"));
-dragElement(document.getElementById("draggable_4"));
-dragElement(document.getElementById("draggable_5"));
-dragElement(document.getElementById("draggable_6"));
-
-
-//Make the function to drag the elements
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "draggable_element")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "draggable_element").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
+// SELECT ALL ELEMENTS THAT CONTAIN AN IMG WITH ID "draggable_element"
+document.querySelectorAll('img#draggable_element').forEach((img) => {
+    const parentDiv = img.parentElement;
+    if (parentDiv) dragElement(parentDiv);
+  });
+  
+  function dragElement(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
     elmnt.onmousedown = dragMouseDown;
+    elmnt.ontouchstart = dragTouchStart;
+  
+    function dragMouseDown(e) {
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+  
+    function dragTouchStart(e) {
+      if (e.touches.length > 0) {
+        pos3 = e.touches[0].clientX;
+        pos4 = e.touches[0].clientY;
+        document.ontouchend = closeDragElement;
+        document.ontouchmove = elementTouchDrag;
+      }
+    }
+  
+    function elementDrag(e) {
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function elementTouchDrag(e) {
+      if (e.touches.length > 0) {
+        pos1 = pos3 - e.touches[0].clientX;
+        pos2 = pos4 - e.touches[0].clientY;
+        pos3 = e.touches[0].clientX;
+        pos4 = e.touches[0].clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      }
+    }
+  
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+      document.ontouchend = null;
+      document.ontouchmove = null;
+    }
   }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+  

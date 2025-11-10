106421edc66a2f81bbb3d808555ded43fc1749ee
salssav <valevelapra@gmail.com>
2025-11-10 10:59:05 +0000

    changing the article photos and green color

--- js/home.js
+++ js/home.js
@@ -94,7 +94,7 @@
             c /= float(MAX_ITER);
             c = 1.17 - pow(c, 1.4);
             vec3 color = vec3(pow(abs(c), 8.0));
-            color = clamp(color + vec3(0.0, 0.04, 0.0), 0.1, 1.0);
+            color = clamp(color + vec3(0.0, 0.0, 0.06), 0.1, 1.0);
             float f = (mouse.z > 0.5 ? 0.2 : 0.01) / length(uv - (vec2(mouse.x, mouse.y)));
             gl_FragColor = vec4(color + f, 1.0);
         }
